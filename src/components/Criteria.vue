a3d29c4ea634019abb96e0c9ac94cccc4e3fe68e
banjac275 <banjac275@gmail.com>
2018-10-12 17:09:22 +0200

    set up project, added minimum layout

---
+++ src/components/Criteria.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-flex xs12 sm2 d-flex class="mx-1">
+    <v-layout column>
+      <v-select
+        :items="items"
+        label="Criteria"
+      ></v-select>
+      <v-input class="input">
+      </v-input>
+      <v-layout>
+        <v-btn>include</v-btn>
+        <v-btn>exclude</v-btn>
+      </v-layout>
+    </v-layout>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ['item']
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  border-bottom: 1px solid #999;
+}
+</style>
